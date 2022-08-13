039732fccce10106fb138dbe52a5fe646d559da1
derrickkim0109 <derrickkim0109@naver.com>
2022-08-13 15:50:50 +0900

    feat: Coredata 다른 버전

---
+++ CoreData_Sample/CoreData_Sample/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  CoreData_Sample
+//
+//  Created by Derrick kim on 2022/08/01.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CoreData_Sample/CoreData_Sample/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CoreData_Sample
+//
+//  Created by Derrick kim on 2022/08/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ViewController {
+    
+}
