e1f45061a3cd5c5a924db430eb6e5f03f9ea031a
derrickkim0109 <derrickkim0109@naver.com>
2022-08-13 15:57:57 +0900

    test: ValueType을 위한 test

---
+++ ValueType_Structure/ValueType_Structure/main.swift
@@ -0,0 +1,40 @@
+//
+//  main.swift
+//  ValueType_Structure
+//
+//  Created by Derrick kim on 2022/07/25.
+//
+
+import Foundation
+
+//func $address(of instance: UnsafeRawPointer) -> String {
+// let address = Int(bitPattern: instance)
+//return String(format: "%p", address)
+// }
+func abc() {
+    var int1 = 100
+    var int2 = 100
+    var int3 = 100
+    
+    var double1 = 100.0
+    var double2 = 200.0
+    var double3 = 300.0
+    
+    var string1 = "abc"
+    
+    var string2 = "def"
+    var string3 = "ghi"
+    var string4 = string1
+    
+    var dic1 = [string1: 1]
+    var dic2 = [string2: 2]
+    var dic3 = [string3: 3]
+
+    var array1 = [1,2,3]
+    var array2 = [4,5,6]
+    var array3 = [7,8,9,9]
+    
+    print("End")
+}
+
+abc()
